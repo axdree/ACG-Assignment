68f219285f899b1a93d63edc669bdc6e36621539
lightcoxa <42603470+lightcoxa@users.noreply.github.com>
2022-02-12 02:57:10 +0800

    update filepath

--- source/PKI/CertificateIssuer.py
+++ source/PKI/CertificateIssuer.py
@@ -17,12 +17,12 @@ password = "6b283bb060c269432d08ac33b47a337c0a40035d"
 scriptpath = os.path.dirname(os.path.abspath(__file__))
 
 #CI's Private key pre-generated
-with open(f"{scriptpath}/private.pem", "rb") as key_file:
+with open(os.path.join(scriptpath, "private.pem"), "rb") as key_file:
     CIprivkey = serialization.load_pem_private_key(
         key_file.read(),
         password=None,
     )
-with open(f"{scriptpath}/CIpublic.pem", "rb") as key_file:
+with open(os.path.join(scriptpath,"CIpublic.pem"), "rb") as key_file:
     CIpubkeyPlainText = key_file.read()
     CIpubkey = serialization.load_pem_public_key(CIpubkeyPlainText)  
     
